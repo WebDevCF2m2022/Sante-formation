506eedabb6f88c00926611eef4769fe8adf3e726
AnamariaTi <tirdeaana@outlook.fr>
2023-06-13 14:07:36 +0200

    class autoloader

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+ use \model\Autoloader;
 
+require '../model/Autoloader.php';
+\model\Autoloader::register();
 require_once '../config.php';
 require_once '../controller/public_controller.php';
