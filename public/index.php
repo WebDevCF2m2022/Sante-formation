e4c0b78bd03e5b96590e4af02624c23adeca6b1c
AnamariaTI <tirdeaana@outlook.fr>
2023-06-13 18:57:54 +0200

    I do the config.php with new value et the try and catch in the index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,26 @@
 <?php
+ session_start();
  use \model\Autoloader;
 
 require '../model/Autoloader.php';
 \model\Autoloader::register();
 require_once '../config.php';
 require_once '../controller/public_controller.php';
+
+
+
+try{
+    $dbConnect = new PDO (DB_TYPE.':host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME.';charset='.DB_CHARSET,
+        DB_USER,DB_PWD);
+    $dbConnect->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(Exception $e){
+    $e = throw new Exception('CONNECTION ERROR');
+
+}
+
+
+
+
+
+
+
