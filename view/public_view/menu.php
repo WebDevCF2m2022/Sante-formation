6e92a23c44c1d58300197bdb933d0ee76537e103
flow3flow <floramine75@hotmail.com>
2023-06-14 11:28:06 +0200

    select language

--- view/public_view/menu.php
+++ view/public_view/menu.php
@@ -1,11 +1,20 @@
-<nav class="navbar">
 
-    <a href="index.php?view=homepage" class="active">L'ASBL</a>
-    <a href="index.php?view=action" class="active">Nos actions</a>
-    <a href="index.php?view=formation" class="active">Formations</a>
-    <a href="index.php?view=agenda" class="active">Tutoriel</a>
-    <a href="index.php?view=contact" class="active">Liens</a>
-    <button><a href="index.php?view=donation" class="active">Faire un don</a></button>
-    <button>Langue</button>
-</nav>
+        <nav class="navbar">
+
+            <a href="index.php?view=homepage" class="active">L'ASBL</a>
+            <a href="index.php?view=action" class="active">Nos actions</a>
+            <a href="index.php?view=formation" class="active">Formations</a>
+            <a href="index.php?view=agenda" class="active">Tutoriel</a>
+            <a href="index.php?view=contact" class="active">Liens</a>
+            <button><a href="index.php?view=donation" class="active">Faire un don</a></button>
+        
+        <select name="langue" id="langue-select">
+            <option value="fr">FR</option>
+            <option value="en">EN</option>
+            <option value="ndl">NL</option>
+            <option value="ar">AR</option>
+        </select>
+        </nav>
+    
+
 
