14b6fee91a6d28be6c9dd8717bf69aab85d2248c
flow3flow <floramine75@hotmail.com>
2023-06-14 11:51:52 +0200

    controler frontal

--- controller/public_controller.php
+++ controller/public_controller.php
@@ -2,3 +2,30 @@
 
 require_once '../view/public_view/homepage.php';
 //include menu dans toute le page
+if(isset($_GET['view'])){
+    switch($_GET['view']){
+        case 'accueil':
+                include "../view/public_view/homepage.php";
+                break;
+        case 'action':
+                include "../view/public_view/action.php";
+                break;
+        case 'agenda':
+                include "../view/public_view/agenda.php";
+                    break;       
+        case 'contact':
+                include "../view/public_view/contact.php";
+                    break;
+        case 'donation':
+                include "../view/public_view/donation.php";
+                    break;
+        case 'formation':
+                 include "../view/public_view/formation.php";
+                    break;
+        
+        default:
+            include_once "../view/public_view/homepage.php";
+    }
+}else{
+    include_once "../view/public_view/homepage.php";
+}
--- view/public_view/menu.php
+++ view/public_view/menu.php
@@ -4,8 +4,8 @@
             <a href="index.php?view=homepage" class="active">L'ASBL</a>
             <a href="index.php?view=action" class="active">Nos actions</a>
             <a href="index.php?view=formation" class="active">Formations</a>
-            <a href="index.php?view=agenda" class="active">Tutoriel</a>
-            <a href="index.php?view=contact" class="active">Liens</a>
+            <a href="index.php?view=agenda" class="active">Agenda</a>
+            <a href="index.php?view=contact" class="active">Contact</a>
             <button><a href="index.php?view=donation" class="active">Faire un don</a></button>
         
         <select name="langue" id="langue-select">
