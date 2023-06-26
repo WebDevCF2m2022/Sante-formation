2336780c89307cc697d79b04453c9cee265db005
Slashyo <tom.bik@hotmail.be>
2023-06-26 11:50:45 +0200

    Page ASBL FINI, nos valeurs à améliorer

--- view/public_view/action.php
+++ view/public_view/action.php
@@ -5,18 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/lightbox.css">
     <link rel="stylesheet" href="css/footer.css">
-  
-    
+    <script src="js/lightbox.js" defer></script>
     <title>Document</title>
 </head>
 <body>
-<!--include menu-->
+<!-- INCLUDE DU MENU -->
     <nav>
         <?php
         include "../view/public_view/src/FR/menuFR.php";
         ?>
     </nav>
+
+    <!-- INCLUDE DU CAROUSEL -->
+<?php
+    include "../view/public_view/src/lightbox.php"
+?>
+
 
  <footer>
      <!-- INCLUDE DU FOOTER -->
