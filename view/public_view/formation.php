17b3a06302a744c13f689b7807fd95081e937b20
flow3flow <floramine75@hotmail.com>
2023-06-27 18:31:46 +0200

    links

--- view/public_view/formation.php
+++ view/public_view/formation.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/menu.css">
     <link rel="stylesheet" href="css/footer.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/countrySelect.css">
+    <script src="js/countrySelect.js"></script>
     
     <title>Document</title>
 </head>
@@ -17,6 +20,14 @@
     ?>
 </nav>
 -
+<script>
+    $("#country_selector").countrySelect({
+        defaultCountry: "fr",
+        onlyCountries: ['gb', 'fr', 'nl', 'sa'],
+        preferredCountries: [],
+        localizedCountries:{'gb': 'GB', 'fr': 'FR', 'nl': 'NL', 'sa': 'AR'}
+    });
+</script>
 <footer>
     <!-- INCLUDE DU FOOTER -->
     <?php
