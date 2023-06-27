d550d2adbaa7a9b7401137c1fa54743a49a565f1
Slashyo <tom.bik@hotmail.be>
2023-06-27 13:49:49 +0200

    Diminution des containers de la page ASBL

--- view/public_view/src/valeur.php
+++ view/public_view/src/valeur.php
@@ -3,31 +3,31 @@
     <div id="valeur1">
         <img src="img/icons8-balancer-64.png" alt="">
         <h4>Titre</h4>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nihil, porro aspernatur, sequi iste cupiditate ipsum quidem similique distinctio in minus vel dignissimos ad ex?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde officia maxime voluptatum dicta itaque repudiandae!</p>
     </div>
     <div id="valeur2">
     <img src="img/icons8-balancer-64.png" alt="">
         <h4>Titre</h4>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nihil, porro aspernatur, sequi iste cupiditate ipsum quidem similique distinctio in minus vel dignissimos ad ex?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde officia maxime voluptatum dicta itaque repudiandae!</p>
     </div>
     <div id="valeur3">
     <img src="img/icons8-balancer-64.png" alt="">
         <h4>Titre</h4>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nihil, porro aspernatur, sequi iste cupiditate ipsum quidem similique distinctio in minus vel dignissimos ad ex?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde officia maxime voluptatum dicta itaque repudiandae!</p>
     </div>
     <div id="valeur4">
     <img src="img/icons8-balancer-64.png" alt="">
         <h4>Titre</h4>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nihil, porro aspernatur, sequi iste cupiditate ipsum quidem similique distinctio in minus vel dignissimos ad ex?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde officia maxime voluptatum dicta itaque repudiandae!</p>
     </div>
     <div id="valeur5">
     <img src="img/icons8-balancer-64.png" alt="">
         <h4>Titre</h4>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nihil, porro aspernatur, sequi iste cupiditate ipsum quidem similique distinctio in minus vel dignissimos ad ex?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde officia maxime voluptatum dicta itaque repudiandae!</p>
     </div>
     <div id="valeur6">
     <img src="img/icons8-balancer-64.png" alt="">
         <h4>Titre</h4>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore nihil, porro aspernatur, sequi iste cupiditate ipsum quidem similique distinctio in minus vel dignissimos ad ex?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde officia maxime voluptatum dicta itaque repudiandae!</p>
     </div>
 </div>
