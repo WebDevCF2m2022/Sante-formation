85400fb344c8d3e8e7f4e4acc707f034f43c83fe
AnamariaTI <tirdeaana@outlook.fr>
2023-06-28 12:11:10 +0200

    donManager

--- model/ManagerClass/DonManager.php
+++ model/ManagerClass/DonManager.php
@@ -12,4 +12,50 @@ use Exception;
 class DonManager implements ManagerInterface
 {
 
+    protected PDO $connect;
+
+    public function __construct(PDO $connection){
+        $this->connect = $connection;
+    }
+
+
+    /**request for on id
+     * @param int $id
+     * @return DonMapping|void
+     */
+    public  function getOneById(int $id){
+        $prepare = $this->connect->prepare("SELECT * FROM don WHERE `idDon` = :id");
+        $prepare->bindValue(":id", $id, PDO::PARAM_INT);
+        try {
+            $prepare->execute();
+            $result = $prepare->fetch();
+            return new DonMapping($result);
+        } catch (Exception $e) {
+            echo "Erreur de requête : " . $e->getMessage();
+            exit;
+        }
+
+    }
+
+    /**
+     * request for get all
+     * @return array
+     */
+    public function getAll(): array{
+        $prepare = $this->connect->prepare("SELECT * FROM don");
+        try {
+            $prepare->execute();
+            $result = $prepare->fetchAll();
+            $all = [];
+            foreach ($result as $row) {
+                $all[] = new DonMapping($row);
+            }
+            return $all;
+        } catch (Exception $e) {
+            echo "Erreur de requête : " . $e->getMessage();
+            exit;
+        }
+    }
+
+
 }
