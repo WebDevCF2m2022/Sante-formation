6c5cfd734587bd84bf431194961e96122d98fed9
AnamariaTI <tirdeaana@outlook.fr>
2023-06-28 13:06:06 +0200

    managerInterface

--- model/InterfaceClass/ManagerInterface.php
+++ model/InterfaceClass/ManagerInterface.php
@@ -1,6 +1,23 @@
 <?php
 
+namespace model\InterfaceClass;
+use model\MappingClass\ActionMapping;
+use model\MappingClass\AgendaMapping;
+use PDO ;
+use Exception;
+
+
+
 interface ManagerInterface
 {
+
+    public function __construct(PDO $connection);
+    public  function getOneById(int $id) ;
+
+    public function getAll();
+
+
+
+
 
 }
