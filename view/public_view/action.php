716bf96b366f561e34dd25553dc975934f913879
Slashyo <tom.bik@hotmail.be>
2023-06-29 10:06:39 +0200

    Page Action terminer (desktop)

--- view/public_view/action.php
+++ view/public_view/action.php
@@ -8,7 +8,8 @@
     <link rel="stylesheet" href="css/lightbox.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/valeur.css">
-
+    <link rel="stylesheet" href="css/contactPage.css">
+    <link rel="stylesheet" href="css/action.css">
     <script src="js/lightbox.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/countrySelect.css">
@@ -17,17 +18,67 @@
     <title>Document</title>
 </head>
 <body>
-<!-- INCLUDE DU MENU -->
+    <div id="gridAction">
+    <!-- INCLUDE DU MENU -->
     <nav>
         <?php
         include "../view/public_view/src/FR/menuFR.php";
         ?>
     </nav>
-
     <!-- INCLUDE DU CAROUSEL -->
-<?php
+    <?php
     include "../view/public_view/src/lightbox.php"
-?>
+    ?>
+
+            <div id="actionBloc">
+                    <h1>Nos actions</h1>
+                    <h3>SLOGAN en long</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, illo?</p>
+            <div id="positionAction">
+                <div class="carteAction">
+                    <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+                    <a href="#"><h1>Actions</h1></a>
+                </div>
+                <div class="carteAction">
+                    <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+                    <a href="#"><h1>Actions</h1></a>
+                </div>
+                <div class="carteAction">
+                    <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+                    <a href="#"><h1>Actions</h1></a>
+                </div>
+                <div class="carteAction">
+                    <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+                    <a href="#"><h1>Actions</h1></a>
+                </div>
+                <div class="carteAction">
+                    <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+                    <a href="#"><h1>Actions</h1></a>
+                </div>
+                <div class="carteAction">
+                    <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+                    <a href="#"><h1>Actions</h1></a>
+                </div>
+                <div class="carteAction">
+                    <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+                    <a href="#"><h1>Actions</h1></a>
+                </div>
+                <div class="carteAction">
+                    <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+                    <a href="#"><h1>Actions</h1></a>
+                </div>
+                <div class="carteAction">
+                    <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+                    <a href="#"><h1>Actions</h1></a>
+                </div>
+            </div>
+            </div>
+
+    <!-- INCLUDE DU CONTACT DANS LA PAGE -->
+    <?php
+    include_once "../view/public_view/src/contactPage.php";
+    ?>
+
 
 <script>
     $("#country_selector").countrySelect({
@@ -38,12 +89,16 @@
     });
 </script>
 
-
+    <!-- INCLUDE DES VALEURS -->
+    <?php
+    include_once "../view/public_view/src/valeur.php";
+    ?>
  <footer>
      <!-- INCLUDE DU FOOTER -->
      <?php
-     include_once '../view/public_view/src/FR/footerFR.php';
+     include_once "../view/public_view/src/FR/footerFR.php";
      ?>
  </footer>
+ </div>
 </body>
 </html>
---
+++ view/public_view/src/contactPage.php
@@ -0,0 +1,17 @@
+<div id="contactPage">
+        <h1>Votre action sur mesure</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta magni officia eveniet maiores consectetur voluptas aliquid quisquam optio, facere repudiandae?</p>
+
+        <form action="#" id="formulairePage" method="post">
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" required><br>
+
+        <label for="email">Email :</label>
+        <input type="email" id="email" name="email" required><br>
+
+        <label for="message">Message :</label>
+        <textarea id="message" name="message" rows="4" cols="30" required></textarea><br>
+
+        <input type="submit" value="Envoyer">
+        </form>
+</div>
