da5a14fcb22b2454b42dbb5d6b542ba5346e269b
flow3flow <floramine75@hotmail.com>
2023-06-30 13:55:33 +0200

    modif css nav

--- view/public_view/src/menu.php
+++ view/public_view/src/menu.php
@@ -3,17 +3,16 @@
 
 <div class="navigation">
 
-   
         <div class="logo">
-        <a id="homepage"href="?view=homepage" class="navi"><img src="img/logoSanteFormation.png" alt="logo" class="img"></a>
-    
+               <a id="homepage"href="?view=homepage" class="navi"><img src="img/logoSanteFormation.png" alt="logo" class="img"></a>
         </div>
+
                <!--<button class="menubtn">hamburgermenuOn</button>-->     
-               <a href="#"class="menubtn"> <img src="img/menu.png" alt="close" class="buttonopen"></a>
-         <!--<button class="menubtn">hamburgermenuOff</button>-->  
-        <a href="#"class="menubtn"> <img src="img/close.png" alt="close" class="buttonclose"></a>
+                 <a href="#"class="menubtn"> <img src="img/menu.png" alt="close" class="buttonopen"></a>
+               <!--<button class="menubtn">hamburgermenuOff</button>-->
+                  <a href="#"class="menubtn"> <img src="img/close.png" alt="close" class="buttonclose"></a>
           
-            <a id="asbl"href="?view=asbl" class="navi">L'ASBL</a>
+            <a id="asbl" href="?view=asbl" class="navi">L'ASBL</a>
             <a id="action" href="?view=action" class="navi">Actions</a>
             <a id="formation" href="?view=formation" class="navi">Formations</a>
             <a id="agenda" href="?view=agenda" class="navi">Agenda</a>
