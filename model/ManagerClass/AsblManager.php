fb18e53a15c10bb24e58a56e143987c597a56814
marshmalocodeuh <100229840+marshmalocode@users.noreply.github.com>
2023-07-12 15:12:36 +0200

    managerasbl

--- model/ManagerClass/AsblManager.php
+++ model/ManagerClass/AsblManager.php
@@ -5,7 +5,7 @@ namespace model\ManagerClass;
 use model\InterfaceClass\ManagerInterface;
 use model\MappingClass\AsblMapping;
 
-use PDO ;
+use PDO;
 use Exception;
 
 
@@ -14,21 +14,31 @@ class AsblManager implements ManagerInterface
 
     protected PDO $connect;
 
-    public function __construct(PDO $connection){
+    public function __construct(PDO $connection)
+    {
         $this->connect = $connection;
     }
-
+    /** 
+     * request get one by id
+     * @param int $id
+     * @return \model\MappingClass\AsblMapping;
+     */
     public function getOneById(int $id)
     {
-        // TODO: Implement getOneById() method.
+        $prepare = $this->connect->prepare("SELECT * FROM asbl WHERE `idasbl` = :id");
+        $prepare->bindValue(":id", $id, PDO::PARAM_INT);
+        try {
+            $prepare->execute();
+            $result = $prepare->fetch();
+            return new AsblMapping($result);
+        } catch (Exception $e) {
+            echo "Erreur de requête : " . $e->getMessage();
+            exit;
+        }
     }
 
     public function getAll()
     {
         // TODO: Implement getAll() method.
     }
-
-
-
-
 }
