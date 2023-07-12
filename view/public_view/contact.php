9537e515a2a0b52951ea88d217b90e16f09d5aa5
flow3flow <floramine75@hotmail.com>
2023-07-12 16:02:53 +0200

    modification contact

--- view/public_view/contact.php
+++ view/public_view/contact.php
@@ -7,8 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/menu.css">
     <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/contactus.css">
+    <link rel="stylesheet" href="css/lightbox.css">
     <script src="js/nav.js" defer></script>
+    <script src="js/lightbox.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>Contact</title>
 </head>
@@ -21,48 +23,41 @@
         ?>
 
     </nav>
+  <!--ligthbox-->
+    <?php
+    include_once "../view/public_view/src/lightbox.php"
+    ?>
+
+
     <div class="container">
-        <div class="pageContact">
-            <div id="boxIntroContact">
 
-
-
-                <h1>Contactez-nous</h1>
-                <p>Une envie de nous envoyer un message ?</p>
-                <p>Une question à nous poser ?</p>
-                <p>Un problème rencontré lors de votre visite ?</p>
-                <p>Veuillez utiliser ce formulaire pour prendre contact avec nous !</p>
-                <p>Je me ferai un plaisir de vous répondre dans les plus brefs délais .</p>
+            <div class ="container1">
+                <h1 class = "contactush1">Contactez-nous</h1>
+                <p class="contactuspar">Une envie de nous envoyer un message ?<br>
+                  Une question à nous poser ?<br>
+                 Un problème rencontré lors de votre visite ?<br>
+                Veuillez utiliser ce formulaire pour prendre contact avec nous !<br>
+               Je me ferai un plaisir de vous répondre dans les plus brefs délais .</p>
             </div>
-            <form action="" method="post" id="contactForm">
-                <div class="boxLabelInputContact">
-                    <label for="firstname">Prénom :</label>
-                    <input type="text" id="firstname" name="firstname" placeholder="" required>
-                </div>
-                <div class="boxLabelInputContact">
-                    <label for="lastname">Nom :</label>
-                    <input type="text" id="lastname" name="lastname" placeholder="" required>
-                </div>
-                <div class="boxLabelInputContact">
-                    <label for="mail">Mail :</label>
-                    <input type="mail" id="email" name="mail" placeholder="" required>
-                </div>
-                <div class="boxLabelInputContact">
-                    <label for="mail"> Categorie :</label>
+        <div class="container2">
+
+            <form action="" method="post" class="formulaire ">
+
+                <label for="lastname" class="contactuslabel"> Nom: </label>
+                    <input type="text" id="lastname" name="lastname" placeholder="" required class="contactusinput">
+                    <label for="mail" class="contactuslabel">Mail :</label>
+                    <input type="mail" id="email" name="mail" placeholder="" required class="contactusinput">
+                    <label for="mail" class="contactuslabel"> Categorie :</label>
                     <select >
                         <option>Administration</option>
                         <option>Donation</option>
                         <option>Benevole</option>
                         <option>Autre</option>
                     </select>
-                </div>
-                <div class="boxLabelInputContact">
-                    <label for="message">Message :</label>
+                    <label for="message" class="contactuslabel">Message :</label>
                     <textarea id="message" name="message" required></textarea>
-                </div>
-                <div id="buttonContact">
-                    <button type="submit">Envoyer mon message.</button>
-                </div>
+                    <button type="submit" class="contactusbutton">Envoyer mon message.</button>
+
             </form>
         </div>
     </div>
