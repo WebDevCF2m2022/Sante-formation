ca269b0f61e7d3524aae54e45e9e77b926e51dcb
Slashyo <tom.bik@hotmail.be>
2023-09-07 13:21:24 +0200

    Quelques amélioration

--- view/public_view/donation.php
+++ view/public_view/donation.php
@@ -7,11 +7,15 @@
     <link rel="icon" href="img/logoSanteFormation.png">
     <link rel="stylesheet" href="css/menu.css">
     <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/lightbox.css">
+    <link rel="stylesheet" href="css/don.css">
     <script src="js/nav.js" defer></script>
+    <script src="js/lightbox.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>Don</title>
 </head>
 <body>
+    <div id="gridDon">
 <!--include menu-->
 <nav>
     <?php
@@ -19,12 +23,17 @@
     ?>
 </nav>
 
+    <?php
+    include_once "../view/public_view/src/lightbox.php"
+    ?>
+
 
 <footer>
     <!-- INCLUDE DU FOOTER -->
     <?php
      include_once "../view/public_view/src/footer.php";
      ?>
+     </div>
 </footer>
 </body>
 </html>
--- view/public_view/homepage.php
+++ view/public_view/homepage.php
@@ -38,31 +38,35 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, suscipit nisi. Quia, incidunt ipsa. Eum voluptates harum animi aut est. Dolore est quasi reiciendis minima natus laudantium officiis? Perferendis distinctio facere quaerat consequuntur? Accusantium quam dicta sint neque ipsa expedita beatae blanditiis sunt necessitatibus dolor itaque voluptatum sed, deserunt, voluptas modi possimus dignissimos dolores temporibus labore, pariatur enim distinctio! Autem magni consequuntur ducimus sed, repudiandae rem nemo omnis illo, esse iure officia placeat neque illum, reiciendis id vel rerum atque a quibusdam nulla corrupti. Rerum in ratione, facere nobis ipsam officiis, velit ipsa suscipit quaerat libero cupiditate deleniti architecto magnam?</p>
 </div>
 
-                <div id="actionAccueil">
-                        <h1>Nos actions</h1>
-                        <h3>SLOGAN en long</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, illo?</p>
-                <div id="positionAction">
-                    <div class="carteAction">
-                        <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
-                        <h1>Actions</h1>
-                    </div>
-                    <div class="carteAction">
-                        <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
-                        <h1>Actions</h1>
-                    </div>
-                    <div class="carteAction">
-                        <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
-                        <h1>Actions</h1>
-                    </div>
-                </div>
-                    <a href="?view=action" target="_blank">En savoir plus</a>
-                </div>
+<div id="actionAccueil">
+        <h1>Nos actions</h1>
+        <!--
+        <h3>SLOGAN en long</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, illo?</p>
+        -->
+<div id="positionAction">
+    <div class="carteAction">
+        <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+        <h1>Actions</h1>
+    </div>
+    <div class="carteAction">
+        <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+        <h1>Actions</h1>
+    </div>
+    <div class="carteAction">
+        <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
+        <h1>Actions</h1>
+    </div>
+</div>
+    <a href="?view=actions" target="_blank">En savoir plus</a>
+</div>
 
 <div id="formationAccueil">
         <h1>Nos formations</h1>
+        <!-- 
         <h3>SLOGAN en long</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, illo?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, illo?</p> 
+        -->
 <div id="positionFormation">
     <div class="carteFormation">
         <img src="img/pexels-jack-redgate-2929227.jpg" alt="">
@@ -77,13 +81,11 @@
         <h1>Formation</h1>
     </div>
 </div>
-    <a href="?view=formation" target="_blank">En savoir plus</a>
+    <a href="?view=formations" target="_blank">En savoir plus</a>
 </div>
 
     <!-- INCLUDE DU CONTACT DANS LA PAGE -->
-    <?php
-    include_once "../view/public_view/src/contactPage.php";
-    ?>
+    
 
 <div id="agendaAccueil">
         <h1 class="centre">Agenda</h1>
