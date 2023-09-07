c4babb43744ee247a09c40bf9d8c149b0d39223a
AnamariaTI <tirdeaana@outlook.fr>
2023-09-07 13:35:44 +0200

    connexion admin avec un error du redirectione

--- controller/public_controller.php
+++ controller/public_controller.php
@@ -1,5 +1,58 @@
 <?php
 
+use model\ManagerClass\AdminManager;
+
+
+try {
+    $pdo = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PWD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
+
+} catch (PDOException $e) {
+    echo "connection error " . $e->getMessage();
+}
+
+
+//conection admin
+if(isset($_POST['submit'])){
+    if(isset($_POST['username']) and !empty($_POST['username'])){
+        if(filter_var($_POST['username'],FILTER_VALIDATE_EMAIL)){
+            if(isset($_POST['pwd']) and !empty($_POST['pwd'])){
+                $pwd = sha1($_POST['pwd']);
+                $getData = $pdo->prepare("SELECT login FROM admin WHERE login =? and pwd =? ");
+                $getData->execute(array($_POST['username'], $pwd));
+                $rows = $getData->rowCount();
+                if($rows ==true){
+                    $_SESSION['admin']=$_POST['username'];
+                    header("Location:../view/public_view/logAdmin.php");
+
+                }else{
+                    $e ="username ou mot de pass inconue  ";
+                }
+
+            }else{
+                $e = "veuillez introduire un  password correct ";
+            }
+
+
+        }else{
+            $e="veuillez saissir un email valide";
+        }
+
+    }
+
+
+}else{
+    $e ="veuillez saissir un username" ;
+
+};
+
+
+
+
+
+
+
 // include menu dans toute la page
 if (isset($_GET['view'])) {
     switch ($_GET['view']) {
@@ -27,6 +80,10 @@ if (isset($_GET['view'])) {
         case 'admin':
             include "../view/public_view/admin.php";
             break;
+
+            case 'logAdmin':
+                include "../view/public_view/logAdmin.php";
+                break;
         default:
             include_once "../view/public_view/homepage.php";
             break;
