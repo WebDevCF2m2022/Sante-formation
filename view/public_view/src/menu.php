07621e265f6e8a9ce7a489a7cf70868fd3a620c9
flow3flow <floramine75@hotmail.com>
2023-09-15 12:09:45 +0200

    changement de langues ds l'URL ok

--- view/public_view/src/menu.php
+++ view/public_view/src/menu.php
@@ -8,27 +8,33 @@
             <div class="bar"></div>
             <div class="bar"></div>
           </div>
+          <?php
+        $URL = isset($_GET['lang'])? '&lang='.$_GET['lang']:"";
+            ?>
           <ul>
-            <li><a href="?view=asbl"><?=$recupNav->getNavbarAsbl()?></a></li>
-            <li><a href="?view=actions"><?=$recupNav->getNavbarAction()?></a></li>
-            <li><a href="?view=formations"><?=$recupNav->getNavbarFormation()?></a></li>
-            <li><a href="?view=agenda"><?=$recupNav->getNavbarAgenda()?></a></li>
-            <li><a href="?view=contact"><?=$recupNav->getNavbarContact()?></a></li>
-            <li><a href="?view=donation"><?=$recupNav->getNavbarDon()?></a></li>
+            <li><a href="?view=asbl<?=$URL?>"><?=$recupNav->getNavbarAsbl()?></a></li>
+            <li><a href="?view=actions<?=$URL?>"><?=$recupNav->getNavbarAction()?></a></li>
+            <li><a href="?view=formations<?=$URL?>"><?=$recupNav->getNavbarFormation()?></a></li>
+            <li><a href="?view=agenda<?=$URL?>"><?=$recupNav->getNavbarAgenda()?></a></li>
+            <li><a href="?view=contact<?=$URL?>"><?=$recupNav->getNavbarContact()?></a></li>
+            <li><a href="?view=donation<?=$URL?>"><?=$recupNav->getNavbarDon()?></a></li>
           </ul>
         </nav>
+        <?php
+        $URL2 = isset($_GET['view'])? 'view='.$_GET['view']:"";
+            ?>
         <div id="langueNavDesktop" class="language-selector">
             <div class="selected-language">
                 <img class="flag-icon" src="img/icons8-french-flag-48.png" alt="French Flag"><p>FR</p><i class="fas fa-caret-down"></i>
             </div>
             <ul class="language-options">
-            <a href="?lang=fr"><li data-lang="fr">
+            <a href="?<?=$URL2?>&lang=fr"><li data-lang="fr">
                 <img class="flag-icon" src="img/icons8-french-flag-48.png" alt="French Flag"><p>FR</p></li></a>
-            <a href="?lang=en"><li data-lang="en">
+            <a href="?<?=$URL2?>&lang=en"><li data-lang="en">
                 <img class="flag-icon" src="img/icons8-great-britain-48.png" alt="American Flag"><p>EN</p></li></a>
-            <a href="?lang=nl"><li data-lang="nl">
+            <a href="?<?=$URL2?>&lang=nl"><li data-lang="nl">
                 <img class="flag-icon" src="img/icons8-netherlands-48.png" alt="American Flag"><p>NL</p></li></a>
-            <a href="?lang=ar"><li data-lang="ar">
+            <a href="?<?=$URL2?>&lang=ar"><li data-lang="ar">
                 <img class="flag-icon" src="img/icons8-saudi-arabia-48.png" alt="American Flag"><p>AR</p></li></a>
             </ul>
         </div>
